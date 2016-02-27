0dd52caf4d2c6933f16dbf2b8285fcc32c96f484
Eric Archambault <1949610+thundernode@users.noreply.github.com>
2016-02-27 18:13:48 -0500

    Removed test echos and silenced output from which (cygwin issue) in gpg-agent.bash.

--- .bashrc.d/gpg-agent.bash
+++ .bashrc.d/gpg-agent.bash
@@ -1,12 +1,9 @@
-if [ $(which gpg-agent) ] ; then
-  #echo "agent exists"
+if [ $(which gpg-agent 2> /dev/null) ] ; then
   if $(grep -qs '^[[:space:]]*use-agent' "${HOME}/.gnupg/gpg.conf" && grep -qs '^[[:space:]]*enable-ssh-support' "${HOME}/.gnupg/gpg-agent.conf") ; then
-    #echo "gpg enabled"
     unset -v SSH_AGENT_PID
     if [ -e "${HOME}/.gpg-agent-info" ]; then
       . $HOME/.gpg-agent-info
       if [ ! -d /proc/$SSH_AGENT_PID ] ; then
-        #echo "pid inactive"
         gpg-agent --daemon --enable-ssh-support --write-env-file "${HOME}/.gpg-agent-info" > /dev/null
         . ${HOME}/.gpg-agent-info
       fi
