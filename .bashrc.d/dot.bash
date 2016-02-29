740ce605af2ed3882f60e9c13ea51717592fa54d
Eric Archambault <1949610+thundernode@users.noreply.github.com>
2016-02-29 15:35:34 -0500

    Added logic for proper autocomplete on the dot script.

--- .bashrc.d/dot.bash
+++ .bashrc.d/dot.bash
@@ -2,3 +2,10 @@
 #is able to use gits autocompletion functions
 source $HOME/.completion/git-completion.bash
 __git_complete dot __git_main
+
+if [ $(type -t _completion_loader) ] ; then
+ eval "$(complete -p git | sed -r 's/(\s)git$/\1dot/')"
+
+ eval "$(type __gitdir |
+ sed '1d;1,/if/s|if|if [[ "$COMP_LINE" == "dot "* ]]; then\necho "$HOME/.dotfiles"\nelif|')"
+fi
